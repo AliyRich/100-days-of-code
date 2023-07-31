4fcc2b7c347721247db63a353fc87d1551bfc0df
Ayodele-hub <aleeyu455@gmail.com>
2023-07-31 13:57:02 +0100

    Day 1

---
+++ script.js
@@ -0,0 +1,20 @@
+// ...100 days of javascript...
+
+// -------- DAY-1 -----------
+
+// Working with javascript functions 
+
+function WELCOME_MESSAGE () {
+
+    let message = "Welcome to the console, i will be logging out my project here: ";
+    console.log(message);
+    
+    // math = a + b;
+    // return math;
+
+}
+
+WELCOME_MESSAGE();
+// console.log(result);
+
+// console.log(message);
