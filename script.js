53b44e150ab305ce983fe1a26ccc97d096e1736f
Ayodele-hub <aleeyu455@gmail.com>
2023-08-05 23:22:31 +0100

    Day 5

--- script.js
+++ script.js
@@ -41,6 +41,9 @@
 
 const prompt = require("prompt-sync")();
 
+
+// Deposit some money
+
 const deposit = () => {
 
     while (true) {
@@ -56,11 +59,13 @@ const deposit = () => {
 }
 }
 
+// Determine number of lines to bet on
+
 const getNumberOfLines = () => {
     while (true) {
         while (true) {
             const lines = prompt("Enter the number of lines to bet on (1-3): ");
-        const numberOfLines = parseFloat(lines);
+            const numberOfLines = parseFloat(lines);
     
         if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
             console.log ("Invalid number of lines, try again");
@@ -71,7 +76,24 @@ const getNumberOfLines = () => {
     }
 }
 }
-const depositAmount = deposit();
-const numberOfLines = getNumberOfLines();
 
+// Collect a bet amount
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance/lines) {
+        console.log ("Invalid bet, try again");
+    }
+    else {
+        return numberBet;
+    }
+ }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
 
